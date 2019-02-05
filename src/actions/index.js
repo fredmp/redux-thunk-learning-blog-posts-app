10215bf28e20e637a00fd50bbf90b3653b7255a7
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-05 19:18:31 -0200

    Avoid duplicated requests when fetching user data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -9,11 +10,37 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = (id) => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(
+    `/users/${id}`
+  );
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
 };
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
+// Avoid duplicated requests by memoizing the action creator
+// (problem here is the content is never updated - it is better to use solution above)
+// export const fetchUser = (id) => async dispatch => {
+//   _memoizedFetchUser(id, dispatch);
+// };
+
+// const _memoizedFetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   });
+// });
